e327a266be7d8a7e67c46e2218eae26ebbababfd
Giovanni Bernardo <canaltamojuntooooo@gmail.com>
2024-12-12 15:17:50 -0300

    first commit

---
+++ src/main/java/acc/br/bean/life/cycle/beans/Weapon.java
@@ -0,0 +1,13 @@
+package acc.br.bean.life.cycle.beans;
+
+public class Weapon {
+    private String name;
+
+    public Weapon(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
